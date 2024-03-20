6f651d8757414d83664b0489a1ae1bb5fa864229
Oxana Chiziţcaia <110328307+CreativeOxana@users.noreply.github.com>
2024-03-20 14:24:11 +0100

    Adding index.html and index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const start = 15;
+
+let delka = 12;
+let konec = 3;
+
+document.body.innerHTML += (start + delka) % 24;
+
+document.body.innerHTML += '<br />';
+
+document.body.innerHTML += 'Běžec doběhne ve: ' + konec;
